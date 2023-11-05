17f837b18e93045fa493b8016c781ccf68d678c0
soheb-funtune <soheb.sayyad@fyntune.com>
2023-11-05 12:47:42 -0800

    min and max digit of array

---
+++ max-and-min.js
@@ -0,0 +1,5 @@
+let arr = [40, 100, 1, 5, 25, 10];
+
+// to find max and min number we will have to spred array inside man() and max() funion/method
+console.log(Math.max(...arr));
+console.log(Math.min(...arr));
