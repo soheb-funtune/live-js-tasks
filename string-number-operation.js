792e9c75890275ac2e431cf4a00fdba9c5c6bdec
soheb-funtune <soheb.sayyad@fyntune.com>
2023-12-05 12:17:43 -0800

    string and number addition

---
+++ string-number-operation.js
@@ -0,0 +1,5 @@
+console.log("logical questions :", 3 + "3"); // 33
+console.log("logical questions :", "3" + 3 - 3); // 30
+console.log("logical questions :", 3 + "3" - 3); // 30
+console.log("logical questions :", "3" + 3 - "3"); // 30
+console.log("logical questions :", 3 + 3 - "3"); // 3
